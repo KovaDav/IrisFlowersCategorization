fde06a3896fb3a245ade2db66756b097c35b6f3e
KovaDav <kovacs.david990420@gmail.com>
2023-06-04 17:31:10 +0200

    program class filled

--- IrisFlowersCategorization/IrisFlowersCategorization/Program.cs
+++ IrisFlowersCategorization/IrisFlowersCategorization/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.ML;
+using IrisFlowerClustering;
+using IrisFlowersCategorization;
 
-Console.WriteLine("Hello, World!");
+string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "iris.data");
+string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "IrisClusteringModel.zip");
+
+var mlContext = new MLContext(seed: 0);
+
+IDataView dataView = mlContext.Data.LoadFromTextFile<IrisData>(_dataPath, hasHeader: false, separatorChar: ',');
+
+string featuresColumnName = "Features";
+var pipeline = mlContext.Transforms
+    .Concatenate(featuresColumnName, "SepalLength", "SepalWidth", "PetalLength", "PetalWidth")
+    .Append(mlContext.Clustering.Trainers.KMeans(featuresColumnName, numberOfClusters: 3));
+    
+var model = pipeline.Fit(dataView);
+
+using (var fileStream = new FileStream(_modelPath, FileMode.Create, FileAccess.Write, FileShare.Write))
+{
+    mlContext.Model.Save(model, dataView.Schema, fileStream);
+}
+
+var predictor = mlContext.Model.CreatePredictionEngine<IrisData, ClusterPrediction>(model);
